8709230e5ba684786ff651fd00da0a5a0186250a
Karina Antonio <karinafantonio@gmail.com>
2017-04-24 04:20:34 -0700

    Implement _treeify_

--- articulation_parse.py
+++ articulation_parse.py
@@ -1,8 +1,82 @@
 import regex
+from treelib import Tree, Node
 
 
 def courses(raw_course_lines):
     to_tokens, from_tokens = _split_tokens_(raw_course_lines)
+    _treeify_(to_tokens).show()
+    _treeify_(from_tokens).show()
+
+
+def _treeify_(tokens):
+    tree = Tree()
+    tree.create_node(tag="AND", identifier="root")
+    operators_being_processed = [{'tag': 'AND', 'identifier': 'root'}]
+    OPERATORS = ("AND", "TO_or", "FROM_or", "&")
+    latest_course_id = ""
+
+
+    def active_operator_id():
+        return operators_being_processed[-1]['identifier']
+
+
+    def precedence_level(op):
+        if not operators_being_processed:
+            return 1
+
+        active_operator = operators_being_processed[-1]['tag']
+
+        if OPERATORS.index(op) < OPERATORS.index(active_operator):
+            return 1
+        elif OPERATORS.index(op) == OPERATORS.index(active_operator):
+            return 0
+        elif OPERATORS.index(op) > OPERATORS.index(active_operator):
+            return -1
+
+
+    def pop_back_to_higher_operator(tag):
+        nonlocal operators_being_processed
+        if any(operator['tag'] == tag for operator in operators_being_processed):
+            while len(operators_being_processed) > 1 and tag != operators_being_processed[-1]['tag']:
+                operators_being_processed.pop()
+            return True
+        else:
+            return False
+
+
+    def put_node_into_operator_subtree(nid, operator_tag):
+        nonlocal tree
+
+        operator = Node(operator_tag)
+
+        tree.add_node(operator,
+                      parent=tree.parent(nid).identifier)
+        tree.move_node(nid, operator.identifier)
+
+        operators_being_processed.append({'tag': operator_tag,
+                                          'identifier': operator.identifier})
+
+
+    for token in tokens:
+        if token in OPERATORS:
+            if precedence_level(token) == 1:
+                if pop_back_to_higher_operator(token):
+                    continue
+                else:
+                    put_node_into_operator_subtree(active_operator_id(), token)
+
+            elif precedence_level(token) == 0:
+                continue
+
+            elif precedence_level(token) == -1:
+                put_node_into_operator_subtree(latest_course_id, token)
+
+        elif _is_course_(token):
+            course = Node(tag=token['code'], data=token)
+            tree.add_node(course, parent=active_operator_id())
+            latest_course_id = course.identifier
+
+    return tree
 
 
 def _split_tokens_(raw_course_lines):
